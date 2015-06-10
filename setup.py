dcd189eb46de1c85031d95c21adcd7f207902304
Andrew Kuchev <0coming.soon@gmail.com>
2015-06-10 16:11:44 +0500

    New release! Again!

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='thread_safe_vk',
-    version='0.1.3',
+    version='0.1.4',
     packages=['thread_safe_vk', 'thread_safe_vk.utils'],
     url='https://github.com/soon/ThreadSafeVk',
     license='MIT',
